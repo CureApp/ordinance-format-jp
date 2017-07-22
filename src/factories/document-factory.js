8e8bd0256a802c5561382f137fb38acf7527bddb
imoans <naomins0101@gmail.com>
2017-07-22 22:49:49 +0900

    :sparkles: [WIP] document factory

--- src/factories/document-factory.js
+++ src/factories/document-factory.js
@@ -1,7 +1,10 @@
 // @flow
 
 import Document from '../elements/document'
-import MarkdownParser from '../parsers/markdown-parser'
+import marked from 'marked'
+import Article from '../elements/article'
+import Element from '../elements/element'
+import Item from '../elements/item'
 
 export default class DocumentFactory {
 
@@ -9,9 +12,68 @@ export default class DocumentFactory {
   }
 
   createFromText(markdownText: string): Document {
-    const parser = new MarkdownParser()
-    const val = parser.parse(markdownText)
-    console.log(val)
+    const tokens = new marked.Lexer().lex(markdownText)
+
+    const doc = new Document()
+    let id = 0
+    let currentElement: Article = new Article({ id })
+    let prevDepth: number
+    let currentDepth: number
+    let items = []
+
+    tokens.forEach(token => {
+      prevDepth = currentDepth || 0
+      currentDepth = token.depth
+
+      // depthが減ったときはdocumentのarticlesに追加
+      if (prevDepth > 1 && prevDepth > currentDepth) {
+        currentElement.items.push(currentElement)
+      }
+
+      // depthが増えたときはdocumentのarticlesに追加
+      if (prevDepth < currentDepth) {
+        doc.articles.push(currentElement)
+        id = id + 1
+        currentElement = new Article({ id })
+      }
+
+      switch(token.type) {
+        case 'heading': {
+          if (currentDepth === 1) {
+            doc.title = token.text
+            break
+          }
+          currentElement.title = token.text
+          break
+        }
+        case 'paragraph': {
+          if (currentDepth === 1) {
+            doc.description = token.text
+            break
+          }
+          currentElement.items.push(token.text)
+          break
+        }
+        case 'list_item_start': {
+          id = id + 1
+          items.push(new Item({ id }))
+          currentElement.items.push(new Item({ id }))
+        }
+        case 'list_item_end': {
+          currentElement.items = items.slice()
+        }
+        case 'text': {
+          items[items.length - 1].statment = token.text
+        }
+        case 'code': {
+          doc.timestamps.push(token.text)
+          break
+        }
+      }
+      // console.log(doc)
+      // console.log(token)
+    })
+
     return new Document({ title: 'タイトル' })
   }
 }
