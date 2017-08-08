d7e04b356582b053fe9a61ac656882b9842ece5d
Ryuichi Sakagami <shagamiiiii@gmail.com>
2017-08-08 18:00:50 +0900

    imple demo

---
+++ test/demo/demo.js
@@ -0,0 +1,14 @@
+import legalDocFormatJp, { format } from '../../src/index.js'
+
+function onChangeText() {
+  const markdownText = document.getElementById("markdownText").value
+  const html = format(markdownText, { standalone: false, elementId: 'corp-site-pp'})
+  document.getElementById("convertedText").innerHTML = html
+}
+
+window.onload = function() {
+  const mT = document.getElementById("markdownText")
+  const cT = format(mT.value, { standalone: false, elementId: 'corp-site-pp' })
+  document.getElementById("convertedText").innerHTML = cT
+  mT.addEventListener("change", onChangeText)
+}
