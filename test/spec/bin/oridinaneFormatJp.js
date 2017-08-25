9c899113ebfeecdecf4a992e993f39c720d33b14
Ryuichi Sakagami <shagamiiiii@gmail.com>
2017-08-25 14:48:20 +0900

    update test

--- test/spec/bin/oridinaneFormatJp.js
+++ test/spec/bin/oridinaneFormatJp.js
@@ -12,42 +12,45 @@ describe('ordinanceFormatJp', function() {
     this.samplePath = resolve(__dirname + '/../../../bin/markdowns/sample.md')
     this.markdownText = fs.readFileSync(this.samplePath, 'utf8')
     this.document = new DocumentFactory().createFromText(this.markdownText)
+    this.oF = new ordinanceFormatJp(this.markdownText)
   })
 
   describe('toHtml',function() {
 
     it('should throw error with unknown options', function() {
-      const oF = new ordinanceFormatJp(this.markdownText)
       const options = { unknown:'unknown', unknown2: true }
-      assert.throws(x => { oF.toHtml(options) })
+      assert.throws(x => { this.oF.toHtml(options) })
     })
 
     it('should be ok with known options', function() {
-      const oF = new ordinanceFormatJp(this.markdownText)
       const options = { standalone: true, elementId:'your id' }
-      assert.doesNotThrow(x => { oF.toHtml(options) })
+      assert.doesNotThrow(x => { this.oF.toHtml(options) })
     })
 
     it('should be ok with empty option', function() {
-      const oF = new ordinanceFormatJp(this.markdownText)
       const options = {}
-      assert.doesNotThrow(x => { oF.toHtml(options) })
+      assert.doesNotThrow(x => { this.oF.toHtml(options) })
     })
 
     it('should throw error when option is object', function() {
-      const oF = new ordinanceFormatJp(this.markdownText)
       const options = { standalone: true, elementId: { elementId: 'your id', elementId: 'your id' }}
-      assert.throws(x => { oF.toHtml(options) })
+      assert.throws(x => { this.oF.toHtml(options) })
+    })
+    
+    it('can outputs formatedHTML', function() {
+      const samplePath = resolve(__dirname + '/../data/sample2.html')
+      const sampleHtml = fs.readFileSync(samplePath, 'utf8')
+      const options = { standalone: false, elementId: 'corp-site-pp'}
+      const formatedHTML = this.oF.toHtml(options)
+      assert(sampleHtml === formatedHTML)
     })
 
     it('should throw error when document is empty ', function() {
-      const oF = new ordinanceFormatJp(this.markdownText)
-      oF.document = ''
+      this.oF.document = ''
       const options = { standalone: true, elementId: 'corp-site-pp'}
-      assert.throws(x => { oF.toHtml(options) })
+      assert.throws(x => { this.oF.toHtml(options) })
     })
-
-
+    
   })
 
   describe('toPlainText', function() {
@@ -55,14 +58,14 @@ describe('ordinanceFormatJp', function() {
     before(function() {
       this.samplePath = resolve(__dirname + '/../../../bin/markdowns/sample.md')
       this.markdownText = fs.readFileSync(this.samplePath, 'utf8')
+      this.oF = new ordinanceFormatJp(this.markdownText)
     })
 
     it('can outputs plain text', function() {
-      const samplePath = resolve(__dirname + '/../../../bin/markdowns/sample.txt')
-      const sampleText = fs.readFileSync(this.samplePath, 'utf8')
-      const oF = new ordinanceFormatJp(this.markdownText)
-      const plainText = oF.toPlainText()
-      assert(plainText === sampleText)
+      const samplePath = resolve(__dirname + '/../data/sample.txt')
+      const sampleText = fs.readFileSync(samplePath, 'utf8')
+      const plainText = this.oF.toPlainText()
+      assert(sampleText === plainText)
     })
   })
 })
